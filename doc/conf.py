cf756cc6d8f4c14cb003c0ac1a4f0be921aa1bbe
wvreeven <wvanreeven@lsst.org>
2020-07-23 13:22:37 +0200

    Fixed package-docs build errors.

--- doc/conf.py
+++ doc/conf.py
@@ -8,6 +8,4 @@ import lsst.ts.GenericCamera
 
 
 _g = globals()
-_g.update(build_package_configs(
-    project_name='ts_GenericCamera',
-    version=lsst.ts.GenericCamera.version.__version__))
+_g.update(build_package_configs(project_name="ts_GenericCamera", version=lsst.ts.GenericCamera.__version__))
